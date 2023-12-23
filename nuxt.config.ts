b0f86c155623ded8c7c3e3be494ea4af66f7659e
Muhammad Zikry Jasman <zikrybee@gmail.com>
2023-12-23 21:03:23 +0700

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
 });
